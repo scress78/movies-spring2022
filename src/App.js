0ecbb8ce76a0295be446a8d3c2254c94627fbf77
scress78 <65985833+scress78@users.noreply.github.com>
2022-04-04 20:54:35 -0500

    Map function and notes

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import './App.css';
 import {movies} from './movies';
-
+//file with same name as component common theme.. note import export
+// parenthesis needed!
 const App = () => {
     return (
         <div>
-			<p>Spencer Cress</p>
+			// pre tag renders code in a code type way
+			// curly brackets signify javascript
+			// for all movies  print the title in a p tag
+			// map function below will be important 
+			// movie is a stand-in for whatever is being looped.
+			// this is like a for loop in python
+			{
+				movies.map((movie) => <p>{movie.title}</p>)
+			}
+			
+			
+			<p>{movies[0].title}</p>
+			<p>{movies[1].title}</p>
+			<p>{movies[2].title}</p>
             <pre>{JSON.stringify(movies, null, 4)}</pre>
         </div>
     );
